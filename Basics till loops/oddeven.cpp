fad2798fa80bf05d31dd907543a5deacfbd281d5
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-01 18:40:23 +0530

    basicstillloops

---
+++ Basics till loops/oddeven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n,i;
+cout<< "enter" ;
+cin>> n;
+for(i=2;i<=2*n;i++)
+{
+    if (i%2==0)
+    {
+        
+    cout<< i;
+    }
+
+}
+return 0;
+}
