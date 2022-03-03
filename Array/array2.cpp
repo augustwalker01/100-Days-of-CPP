c7e0766ab85568ff4b49a8152fd880abbe36a434
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-03 23:06:12 +0530

    array2.cpp

--- Array/array2.cpp
+++ Array/array2.cpp
@@ -1,11 +1,20 @@
+//for taking elemnts from user and printing them (array)
 #include<iostream>
 using namespace std;
 int main()
 {
-    float A[]={2.3,0.6,4,6};
-    for(auto x:A)
-    {
-        cout<<x<<endl;
-    }
+   int A[4];
+   cout<<"enter the elements"<<endl;
+   for (int i = 0; i < 4; i++)
+   {
+       cin>>A[i];
+   }
+   for ( int j = 0; j< 4; j++)
+   {
+   cout<<A[j]<<endl;
+   }
+   
+   
+  
     return 0;
 }
