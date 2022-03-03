91b0375e54381965b05c08dbbd0c35e0e5fe05c2
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-03 11:04:16 +0530

    Pointers

---
+++ .vscode/Pointers/pointers1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x=10;
+int*P;//DECLARATION
+P=&x;//INITIALIZATION
+
+cout<<x<<endl;
+cout<<&x<<endl;
+cout<<*P<<endl;//DEREFERENCING
+cout<<P;
+return 0;
+}
