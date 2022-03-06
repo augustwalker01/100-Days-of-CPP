26ca2cea19c0d1e736deca33811d7ba4b369d825
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-06 22:14:56 +0530

    meanofnumbers

---
+++ Array/mean.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int m,e,sum=0;
+cout<<" how many elements are there?"<<endl;
+cin>>e;
+cout<<" Enter the numbers for calculating mean"<<endl;
+for(int i=1;i<=e;i++)
+{
+    cin>>m;
+    sum+=m;
+}
+
+int mean= sum/e;
+cout<<mean;
+
+return 0;
+}
