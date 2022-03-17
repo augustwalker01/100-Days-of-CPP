4481d5922b84cfd78d173268783f02f34e847f99
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-17 22:06:06 +0530

    .vscode

---
+++ .vscode/Functions/fntemplate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+template <class T>
+T maxim(T a,T b)
+
+{
+    return a>b?a:b;
+}
+
+int main()
+{
+cout <<maxim(45,7)<<endl;
+cout <<maxim(5.8,9.8)<<endl;
+cout<<maxim(7.8f,8.9f);
+
+return 0;
+}
