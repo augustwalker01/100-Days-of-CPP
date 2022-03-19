aca0b38e464bc1756b7414c196d68068196f80ee
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-03-19 15:43:49 +0530

    .vscode

---
+++ .vscode/Functions/defaultarguements.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+float add(float x ,float y=0,float z=0)
+//^| this tells us what will be returned regardless of what's in the bracket
+{
+    return x+y+z;
+}
+
+
+int main(){
+    cout<<add(10.6);
+    cout<<add(10,6);
+    cout<<add(4.76,12,2.888);
+
+return 0;
+}
