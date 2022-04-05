939e12437dbb78ccd372a371caa85cc642b925e8
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-04-05 23:23:24 +0530

    modified

---
+++ Practice/practice3.cpp
@@ -0,0 +1,9 @@
+//a program in C++ to check the upper and lower limits of integer.
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"The maximum and minimum limit of int "<< INT_MAX <<" and "<<INT_MIN;
+    cout<<"The maximum and minimum limit of CHAR "<< CHAR_MAX <<" and "<<CHAR_MIN;
+
+return 0;
+}
