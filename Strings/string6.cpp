6ec8ee979e30d17f00143c76d273b5cc0519645a
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-04-15 16:16:51 +0530

    new

---
+++ Strings/string6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str1="abc";
+    string str2="abc";
+    if( str1.compare(str2)==0)
+
+    {
+        cout<<"The string entered are same"<<endl;
+    }// main use of this string function is to compare two strings and get to know that strings entered are same or different
+else
+cout<<"string not same"<<endl;
+   
+
+
+return 0;
+
+}
