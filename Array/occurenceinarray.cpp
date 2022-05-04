91bdb816ee2049eeb87755210ae4f94f656f948c
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-05-05 00:01:46 +0530

    new

--- Array/occurenceinarray.cpp
+++ Array/occurenceinarray.cpp
@@ -1,27 +1,40 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
+    int a,b,i,j, x;
 int arr[10]={1,1,2,2,2,2,2,4,4,5};
+
+
 cout<<"enter the element"<<endl;
 cin>>x;
-for ( int i = 0; i <= 9; i++)
+for ( int i = 0; i >= 9; i++)
 {
    if (arr[i]==x)
    {
-       cout<<"The first index is"<<i;
-       if (arr[i]==x)
-       {
-           cout<<"the second index is "<<i;
-       }
+       i=a;
+       
       
        
    }
    else
    {
-       cout<<"not found"<<endl;
+       cout<<"not found";
    }
+ 
 }
+cout<<"the first occurence is at index "<<a<<endl;
+while ( j!=0)
+{
+if (arr[j]==x)
+   {
+       j=b;
+   }
+
+   
+   
+}
+
+cout<<"The last occurence is at index "<<b;
 
 
 return 0;
