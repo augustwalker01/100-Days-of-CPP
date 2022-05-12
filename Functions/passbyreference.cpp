f369e831057071a58b122dfe5a03f02d7428d969
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-05-12 21:50:50 +0530

    new

--- Functions/passbyreference.cpp
+++ Functions/passbyreference.cpp
@@ -5,10 +5,13 @@
      cout<<&a<<" "<<&b<<endl;
     int temp=a;
     a=b;
+
+    
     b=temp;
     cout<<&a<<" "<<&b<<endl;
  }
  int main(){
+
      int x=13, y=23;
  swap(x,y);
  cout<<x<<" "<<y<<endl;
--- Functions/passbyvalue.cpp
+++ Functions/passbyvalue.cpp
@@ -8,6 +8,7 @@ void swap (int a , int b )
     b=temp;
     cout<<a<<" "<<b;
     
+
 }
 
 int main(){
