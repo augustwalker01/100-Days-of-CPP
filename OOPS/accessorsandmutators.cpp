7e2373d4295fb33d7d185bec531de802be31637a
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-05-20 19:33:21 +0530

    new

---
+++ OOPS/accessorsandmutators.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+    private:
+int length;
+int breadth;
+public:
+int area(){
+    return lengthbreadth 
+}
+
+int perimeter(){
+    return 
+}
+
+
+}
+
+
+int main(){
+
+return 0;
+}
