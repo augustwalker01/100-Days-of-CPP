1bf438195df1b89dd6b2536b4df635e0e72f3ae1
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-06-23 22:26:00 +0530

    first comm

--- Basic C#/program1.cs
+++ Basic C#/program1.cs
@@ -1,12 +1,18 @@
-using System;
+using System;// means that we can use classes from the System namespace.
 
-namespace HelloWorld
+
+namespace HelloWorld // namespace is used to organize your code, 
+//and it is a container for classes and other namespaces.
+// A blank line. C# ignores white space. However, multiple lines makes the code more readable.space HelloWorld
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");    /*Console is a class of the System namespace, 
+      which has a WriteLine() method that is used to output/print text. In our example it will output "Hello World!".*/
+      
+      
     }
   }
 }
