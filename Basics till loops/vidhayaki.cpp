bfadc5e34713cf569999a662964e4b8c0967bbf2
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-07-05 20:36:59 +0530

    commit

---
+++ Basics till loops/vidhayaki.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+double price, radius,height,volume,litres;
+
+cout<<"enter radius and height"<<endl;
+
+cin>>radius>>height;
+volume= 3.14*radius*height;
+//cout<<volume<<"cm^3"<<endl;
+litres=volume/1000;
+//cout<<litres<<"litres"<<endl;
+price=40*litres;
+cout<<price;
+return 0;
+}
