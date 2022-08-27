c93e6fecf3edbc80935e1252b105136dc8ce607c
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-08-28 00:15:51 +0530

    commit

--- Practice/recursion1.cpp
+++ Practice/recursion1.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
+int  fact(int n){
+    
+    if(n==0){
+   return 1;
+  
+    }
+    int r;
+     r=n* fact(n-1);
+   return r;
+}
 int main(){
-
+cout<<fact(4);
 return 0;
 }
