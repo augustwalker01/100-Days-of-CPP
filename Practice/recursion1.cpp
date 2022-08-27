a1755bf3705660c6867b99b59179f0dd4a4d3ecc
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-08-28 00:00:28 +0530

    commit

--- Practice/recursion1.cpp
+++ Practice/recursion1.cpp
@@ -1,15 +1,6 @@
 #include<iostream>
 using namespace std;
-void natural(int n){
-    natural(n+1);
-    if(n==50){
-        return 0;
-    }
+int main(){
+
+return 0;
 }
-int main()
-
-
-{
-    cout<<natural(n=1);
-}
-
