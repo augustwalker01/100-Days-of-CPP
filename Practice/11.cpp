c05c06a41bb2c0ef9c3b1ab12c982651c26597fb
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-08-29 23:04:32 +0530

    commit

---
+++ Practice/11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t,a,b,c;
+cin>>t;
+while(t--)
+{
+    cin>>a>>b>>c;
+    if((a+b)/2>c){
+        cout<<"YES"<<endl;
+    }
+    else
+    cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
