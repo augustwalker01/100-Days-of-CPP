1ae4c67bd35ba2ba996781e44fa642aea3968863
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-09-19 21:25:42 +0530

    commit

---
+++ STL/vector.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<v.capacity()<<endl;
+    v.push_back(2);
+    cout<<v.capacity()<<endl;
+    v.push_back(3);
+    cout<<v.capacity()<<endl;
+    cout<<v.size()<<endl;
+    cout<<v.at(2)<<endl;
+    cout<<v.front()<<endl;
+    cout<<v.back()<<endl;
+
+cout<<"before pop"<<endl;
+for(int i:v){
+    cout<<i<<" ";
+}cout<<endl;
+v.pop_back();
+cout<<"after popping"<<endl;
+for(int i:v){
+    cout<<i<<" ";
+}
+v.clear();
+cout<< "after clear"<<v.size()<<endl;
+
+
+return 0;
+}
